c6c0240d895b9683f5d6a7e0a67ae9997c5627d0
Gustavo Souza <gdssouza@id.uff.br>
2021-10-28 08:25:07 -0300

    feat: convolution function

--- src/synthetic_signal_recovery.py
+++ src/synthetic_signal_recovery.py
@@ -5,3 +5,11 @@ def ricker(f, length, dt):
     t0 = np.arange(-length/2, (length-dt)/2, dt)
     y = (1.0 - 2.0*(np.pi**2)*(f**2)*(t0**2)) * np.exp(-(np.pi**2)*(f**2)*(t0**2))
     return t0, y
+
+def convolution(Rc_tdom, f, length, dt):
+    f=20            #wavelet frequency
+    length=0.512    #Wavelet vector length
+    dt=dt           # Sampling prefer to use smiliar to resampled AI
+    t0, w = ricker (f, length, dt) # ricker wavelet 
+    synthetic = np.convolve(w, Rc_tdom, mode='same')
+    return synthetic
