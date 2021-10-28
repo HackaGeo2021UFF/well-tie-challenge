d9b9d1aad433d4e019baab1ef8660973a2337f40
Gustavo Souza <gdssouza@id.uff.br>
2021-10-28 08:21:53 -0300

    feat: ricker function

--- src/synthetic_signal_recovery.py
+++ src/synthetic_signal_recovery.py
@@ -1,1 +1,7 @@
+import numpy
 
+# define function of ricker wavelet
+def ricker(f, length, dt):
+    t0 = np.arange(-length/2, (length-dt)/2, dt)
+    y = (1.0 - 2.0*(np.pi**2)*(f**2)*(t0**2)) * np.exp(-(np.pi**2)*(f**2)*(t0**2))
+    return t0, y
