d13a84d407b728e4d62b0f9bad0b884a77cdec19
Leonardo Miquelutti <lmiquelutti@id.uff.br>
2021-10-28 16:13:15 -0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,13 +1,39 @@
-'''Esse é apenas um '.py' teste.'''
+'''
+This file automates the well-tieing process
 
-import numpy as np
-import matplotlib.pyplot as plt
+Usage is:
 
-x= np.arange(0,10,0.1)
-y = np.exp(x)
+"python grad_uff_solucao.py inputs.json"
 
-print(y)
+Arguments
+    inputs.json: string
+      the json file with its inputs
+'''
 
-#plt.figure(figsize=(5,5))
-plt.plot(x,y)
-plt.show()
+import welly
+import las
+
+# user inputs `ui` passed as a json file
+ui = read_inputs(sys.argv[1])
+
+# read data defined in the input file
+data = read_data(ui)
+
+# pre-processing of data
+data = pre_processing_data(data)
+
+# time-depth relationship `tdr` from DT
+data = time_depth_relationship(data)
+
+# reflectivity coefficients `rc` profile (in time)
+data = rc_time(data)
+
+# convolution of the wavelet with `rc` to obtain
+# the synthetic seismogram
+data = wvlt_conv(data)
+
+# evaluate the recovered signal
+score = evaluate_results(data)
+
+# export data to Decision Workspace
+export_data(data)
