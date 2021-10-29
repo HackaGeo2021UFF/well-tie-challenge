83a1ad9bc409e032e8411bb0cb437c8d1dc4e0a8
Gustavo Souza <gdssouza.up@gmail.com>
2021-10-29 14:56:01 -0300

    feat: read data

--- src/well_tie.py
+++ src/well_tie.py
@@ -1,4 +1,5 @@
 import json
+from welly import Well
 
 def read_inputs(jpath):
     with open(jpath) as file:
@@ -6,7 +7,28 @@ def read_inputs(jpath):
     return paths
 
 def read_data(ui):
-    return None
+    '''
+    Description
+        
+    Arguments
+        path: string
+    Returns
+        well: welly.well.Well
+    '''
+    
+    # read well .las
+    well = Well.from_las(ui['well_path'])       
+
+    # read from csv file into dataframe
+    df_top = pd.read_csv(ui['tops'])            
+    # convert to dictionary
+    tops_dict = dict(df_top.values.tolist())    
+
+    # read seismic arround well
+    seismic_df = pd.read_csv(ui['seismic'])     
+
+    data = {'well':well,'tops':tops_dict,'seismic':seismic_df}
+    return data
 
 def pre_processing_data(data):
     return None
