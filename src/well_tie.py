a862597e087b67ac46a34e5690e62f702774b189
Gustavo Souza <gdssouza.up@gmail.com>
2021-10-29 16:11:09 -0300

    feat: pre processing

--- src/well_tie.py
+++ src/well_tie.py
@@ -1,4 +1,5 @@
 import json
+import welly
 from welly import Well
 
 def read_inputs(jpath):
@@ -31,23 +32,44 @@ def read_data(ui):
     # read seismic arround well
     seismic_df = pd.read_csv(ui['seismic'])     
 
-    data = {'well':well,'tops':tops_dict,'seismic':seismic_df}
+    data = {'well':well.data,'tops':tops_dict,'seismic':seismic_df}
     return data
 
 def pre_processing_data(data):
-    return None
+    #unit convert to µs/m
+    data['well']['DT'] = data['well']['DT'] / 0.3048  
+    #unit convert to kg/m3  
+    data['well']['RHOB'] = data['well']['RHOB'] * 1000        
+    #Despiking
+    #Sonic Despiking
+    dt = data['well']['DT']
+    data['well']['DT_DS'] = dt.despike(window_length=50, z=2)
 
-def time_depth_relationship(data):
-    return None
+    #Density Despiking
+    den = data['well']['RHOB']
+    data['well']['RHOB_DS'] = den.despike(window_length=50, z=2)
+
+    #Smoothing 
+    #Sonic Smoothing
+    dt_ds = data['well']['DT_DS']
+    data['well']['DT_DS_SM'] = dt_ds.smooth(window_length=10, samples=False)
+
+    #Density Smoothing
+    den_ds = data['well']['RHOB_DS']
+    data['well']['RHOB_DS_SM'] = den_ds.smooth(window_length=10, samples=False)
+    return data
+
+def time_depth_relationship(data, ):
+    return data
 
 def rc_time(data):
-    return None
+    return data
 
 def wvlt_conv(data):
-    return None
+    return data
 
 def evaluate_results(data):
     return data
 
 def export_data(data):
-    return None
+    return data
