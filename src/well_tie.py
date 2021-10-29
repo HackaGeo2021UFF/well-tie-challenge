c00b1c0bab64a720cd1e5b6fb1b486393d59c8a4
Gustavo Souza <gdssouza.up@gmail.com>
2021-10-29 13:53:40 -0300

    feat: creat empty well_tie.py

---
+++ src/well_tie.py
@@ -0,0 +1,23 @@
+def read_inputs(jpath):
+    return None
+
+def read_data(ui):
+    return None
+
+def pre_processing_data(data):
+    return None
+
+def time_depth_relationship(data):
+    return None
+
+def rc_time(data):
+    return None
+
+def wvlt_conv(data):
+    return None
+
+def evaluate_results(data):
+    return data
+
+def export_data(data):
+    return None
