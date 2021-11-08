492dacce62281b34092aff14a3ab133f4363e24c
Gustavo Souza <gdssouza.up@gmail.com>
2021-11-08 17:17:52 -0300

    fix: rolls

--- src/waveletChoice.py
+++ src/waveletChoice.py
@@ -41,45 +41,45 @@ def evaluate_results(tr_synth, tr_seis):
 
     return r[0][1]
 
-def matching(tr_seis, tr_synth):
-    
+def matching(tr_synth, tr_seis):
     tr_seis_new = tr_seis.copy()
-
     i = 0
     while tr_synth[i] == 0 and i < len(tr_seis):
         tr_seis_new[i] = 0
         i += 1
-
     i = len(tr_seis)-1
-    while tr_synth[i] != 0 and i >= 0:
+    while tr_synth[i] == 0 and i >= 0:
         tr_seis_new[i] = 0
         i -= 1
-
     return tr_seis_new 
 
 def best_wavelet(data):
     
     freqs = np.arange(5, 50+0.2, 0.2)
-    corr_index = 0
+    rolls = np.arange(-10,101)
+    tr_seis = data['seismic']['tr_seis'].to_numpy()
     Rc_tdom = data['well_tdom']['Rc_tdom']
+    best_corr = None
 
     for freq in freqs:
 
         wavelet = ricker(freq, data)
-        tr_seis = data['seismic']['tr_seis'].to_numpy()
         tr_synth = np.convolve(wavelet, Rc_tdom, mode='same')
         
-        for r in range(-10, 10):        #roll
+        for r in rolls:        #roll
 
             tr_synth_roll = np.roll(tr_synth, r)
-            tr_seis_match = matching(tr_seis, tr_synth)
-            corr = np.corrcoef(tr_synth_roll, tr_seis)[0][1]
+            tr_seis_match = matching(tr_synth, tr_seis)
+            corr = evaluate_results(tr_synth_roll, tr_seis_match)
             
-            if corr > corr_index:
+            if best_corr == None:
                 best_corr = corr
                 best_roll = r
                 best_freq = freq
-                corr_index = best_corr
+            elif corr > best_corr:
+                best_corr = corr
+                best_roll = r
+                best_freq = freq
     
     print("Best wavelet: f = %.2f, index roll = %i and corr = %.3f"%(best_freq, best_roll, best_corr))
 
